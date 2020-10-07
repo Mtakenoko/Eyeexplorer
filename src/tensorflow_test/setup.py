de5d9a2eeffc5c0398498b82194624aa44bfb2cc
Hayato Takeyama <takehaya.724@gmail.com>
2020-10-07 13:09:52 +0900

    make tensorflow_test pkg

---
+++ src/tensorflow_test/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages
+from setuptools import setup
+import os
+from glob import glob
+
+### ここにパッケージ名を記述 ###
+package_name = 'tensorflow_test'
+
+### 必要に応じてパッケージ情報を記述 ###
+setup(
+    name=package_name,
+    version='0.7.8',
+    packages=[],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Hayato Takeyama',
+    author_email='takehaya.724@gmail.com',
+    maintainer='Hayato Takeyama',
+    maintainer_email='takehaya.724@gmail.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'pubsubpy sample'
+    ),
+    license='MIT License',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pub = node.Pub:main',
+            'sub = node.Sub:main',
+        ],
+    },
+)
+### ↑ entry_pointsの部分に、パッケージに含まれるノード(pythonソース)を列挙 ###
